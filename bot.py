3e272da1dc7dcfed1d8b5361b09fa596d41efff7
jskalaknzma7282 <hansna@inbox.ru>
2026-04-01 23:52:07 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import asyncio
 import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=BOT_TOKEN)
@@ -24,7 +24,7 @@ keyboard = ReplyKeyboardMarkup(
 @dp.message(Command("start"))
 async def start(message: types.Message):
     await message.answer(
-        "Добро пожаловать в GSM👋\n"
+        "Добро пожаловать в GSM👋\n\n"
         "<i>Выберите действие:</i>",
         parse_mode="HTML",
         reply_markup=keyboard
@@ -40,7 +40,22 @@ async def links(message: types.Message):
 
 @dp.message(lambda message: message.text == "Поддержка")
 async def support(message: types.Message):
-    await message.answer("Заглушка: Поддержка. В разработке")
+    inline_keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(
+                text="Поддержка >",
+                url="https://t.me/switchsupp",
+                style="primary"
+            )
+        ]
+    ])
+    
+    await message.answer(
+        "<b>🚨 Поддержка/Вопросы</b>\n\n"
+        "Если возникли баги с ботом, или проблемы по оплате/перевыдаче обратитесь к администратору. Отвечаем в течение 24 часов.",
+        parse_mode="HTML",
+        reply_markup=inline_keyboard
+    )
 
 async def main():
     await dp.start_polling(bot)
